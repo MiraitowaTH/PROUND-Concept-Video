d9628f7eaa73536f0265f2fce216d9b22c201b8c
MiraitowaTH <saonoibnk48fanclub@gmail.com>
2024-08-30 00:37:15 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function tweetMessage() {
-    const message = "MIRAI TO WA PROUND\n#MiraitowaTH #ProundBNK48 ";
+    const message = "MIRAI TO WA PROUD\n#MiraitowaTH #ProudBNK48 ";
     const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(message)}`;
     window.open(tweetUrl, '_blank');
 } 
